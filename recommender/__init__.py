fddb990bd65b0fefc454058742ab96e352940c1e
Alaaokaly <111387490+Alaaokaly@users.noreply.github.com>
2025-04-10 22:55:12 +0200

    Delete recommender directory

--- recommender/__init__.py
+++
@@ -1,7 +0,0 @@
-#from .content_based import ContentBasedRecommender
-from .CF_KNN_user_based import UserBasedCF
-from .CF_KNN_item_based import ItemBasedCFRecommender
-from .hybird import HybridRecommender
-# ## from .CF_SVD import 
-#can limit to func or class
-__all__ = ["ContentBasedRecommender", "UserBasedCF","ItemBasedCFRecommender", "HybridRecommender"]
