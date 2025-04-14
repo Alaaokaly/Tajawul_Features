6efd43ec8e9b55f7f2ec2946b5be203da5bca551
= <=alaa.okaly410@gmail.com>
2025-04-15 00:20:39 +0200

    text comments for  requirements for testing

--- recommender_engine/recommender/testing.py
+++ recommender_engine/recommender/testing.py
@@ -37,3 +37,40 @@ def calculate_coverage(model):
     item_coverage = float(no_items_in_rec/ no_items)
     return user_coverage, item_coverage
 
+""" Evaluation :
+                - Offline 
+                - Online 
+                - controlled group 
+"""
+
+""" Offline Evaluation : 
+                        - split data  verify on one set that the recommender predicts items that
+                           are close to what the user actually chooses in the hidden set 
+                        -  see if the recommender can predict the type of interaction the user had with the item
+                        -  test the predictions by the hidden set 
+                        - measuer error/decision  matrix 
+                        -  
+                              """
+
+""" Precision : number of relevent items between k-items/users 
+               relevant item  can be an item that has higher frequency in relations than others
+                - you want relevent items to be recommended more 
+       
+                  
+    The average precision (AP) :
+                               can be used to measure how good the rank is by running
+                               the precision from 1 to m, where m is the number of items 
+                               that are recommended (usually denoted as k) 
+                                works per recommendation, so if you want to use it as a
+                                measure to evaluate a recommender, you can then take the 
+                                mean of the average (or mean average precision—MAP) over 
+                                all recommendations
+                                  
+                                    
+   discounted cumulative gain (DCG):
+                                   Its about finding each relevant item and then penalizing 
+                                   the items the farther down itis in the list.   
+                                   the DCG looks at levels of relevancy.
+                                   you give each item a relevancy score. 
+                                   it can be the predicted rating or profit on the item. 
+                                     """
